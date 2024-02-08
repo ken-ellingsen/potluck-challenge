65828c311508f6688c9b37200a75153a9a4b327b
Ken Ellingsen <152531539+ken-ellingsen@users.noreply.github.com>
2024-02-08 16:26:45 -0600

    Added button and guest list functionality

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
-// invite button
-const addGuestButton = document.querySelector(".invite");
-// label for the invite button
-const guestInputLabel = document.querySelector(".add-guest label");
-// text input box
-const guestInput = document.querySelector(".add-guest input");
-// unordered list (not yet visible)
-const guestList = document.querySelector(".guest-list");
-// span class for number of guests attending
-const guestCount = document.querySelector(".attendance");
-// alert when guest list is full (not yet visible)
-const guestFull = document.querySelector(".alert");
+const addGuestButton = document.querySelector(".invite"); // invite button
+const guestInputLabel = document.querySelector(".add-guest label"); // label for the invite button
+const guestInput = document.querySelector(".add-guest input"); // text input box
+const guestList = document.querySelector(".guest-list"); // unordered list (not yet visible)
+const guestCount = document.querySelector(".attendance"); // span class for number of guests attending
+const guestFull = document.querySelector(".alert"); // alert when guest list is full (not yet visible)
+
+addGuestButton.addEventListener("click", function () {
+    const guest = guestInput.value;
+    if (guest !== "") {
+        addToList(guest);
+        updateGuestCount();
+        clearInput();
+    }
+});
+
+const clearInput = function () {
+    guestInput.value = "";
+};
+
+const addToList = function (guest) {
+    const listItem = document.createElement("li");
+    listItem.innerText = guest;
+    guestList.append(listItem);
+};
+
+const updateGuestCount = function () {
+    const guests = document.querySelectorAll(".guest-list li");
+    guestCount.innerText = guests.length;
+
+    if (guests.length === 8){
+        addGuestButton.classList.add("hide");
+        guestInput.classList.add("hide");
+        guestInputLabel.classList.add("hide");
+        guestFull.classList.remove("hide");
+    }
+};
